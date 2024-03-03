8fd72881d3d4215a3469531d9935059a03273476
SIK-NET <github@sik-net.com>
2024-03-03 00:58:32 -0300

    Replace the For loop
    replace the FOR loop by a counter in the FIND fonction

--- Zabbix-Templates/Operating_Systems/Windows/template_rdp_active_users_fr/6.0/RDPActiveUsers.cmd
+++ Zabbix-Templates/Operating_Systems/Windows/template_rdp_active_users_fr/6.0/RDPActiveUsers.cmd
@@ -3,6 +3,4 @@
 Set CurrentServer=%COMPUTERNAME%
 
 :: Script
-Set Active=0
-for /F "usebackq tokens=1,2,3,4,5*" %%i in (`qwinsta /server:"%CurrentServer%" ^| find "Actif"`) do if "%%l" == "Actif" (set /a Active=Active+1)
-echo %Active%
+qwinsta /server:"%CurrentServer%" | FIND /C "Actif"
